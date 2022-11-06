7d088ce3f3067416be2d9ba610f865638d19e5c1
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2022-11-05 22:07:01 -0700

    add conditional statements for langaugePicker function

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 //business logic
 function languagePicker(event) {
   event.preventDefault();
+  //define input values
   const input1 = document.querySelector("input[name='question1']:checked").value;
   const input2 = document.querySelector("input[name='question2']:checked").value;
   const input3 = document.querySelector("input[name='question3']:checked").value;
   const input4 = document.querySelector("input[name='question5']:checked").value;
-  if () {
-
-  } else if () {
-
-  } else if () {
-
+  
+  if (input1 === "multi" && input4 === "generalist") {
+    document.querySelector("div#result3").removeAttribute("class");
+  } else if (input2 === "low" || input2 === "generalist") {
+    document.querySelector("div#result2").removeAttribute("class");
+  } else if (input3 === "game") {
+    document.querySelector("div#result1").removeAttribute("class");
   }else {
-
+    document.querySelector("div#result4").removeAttribute("class");
   }
 }
 
