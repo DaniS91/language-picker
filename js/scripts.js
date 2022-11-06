c3581fc0a4870a519b9dbf98749f2c866041d846
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2022-11-05 21:56:26 -0700

    WIP: add basic js logic flow

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+//business logic
+function languagePicker(event) {
+  event.preventDefault();
+  const input1 = document.querySelector("input[name='question1']:checked").value;
+  const input2 = document.querySelector("input[name='question2']:checked").value;
+  const input3 = document.querySelector("input[name='question3']:checked").value;
+  const input4 = document.querySelector("input[name='question5']:checked").value;
+  if () {
+
+  } else if () {
+
+  } else if () {
+
+  }else {
+
+  }
+}
+
+//user interface logic
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("quiz");
+  form.addEventListener("submit", languagePicker)
+
+})
